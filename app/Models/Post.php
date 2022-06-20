acb8aaed435a7c4bb2f99baa515a3aae79559ce2
Mohammad Tumeh <mhamadtumeh123123@gmail.com>
2022-06-20 16:00:54 +0300

    Finish Post And Post Gallery

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'text', 'date', 'service_provider_id'
+    ];
+
+    public function service_provider()
+    {
+        return $this->belongsTo(ServiceProvider::class);
+    }
+
+    public function posts_gallery()
+    {
+        return $this->hasMany(PostsGallery::class);
+    }
 }
--- app/Models/PostsGallery.php
+++ app/Models/PostsGallery.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class PostsGallery extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'title', 'image', 'post_id'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
