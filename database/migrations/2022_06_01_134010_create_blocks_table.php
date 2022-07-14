a10df67576a8a1ae687186090a42beb9ef49b3e9
unknown <bkerkz99@gmail.com>
2022-07-14 12:28:41 +0300

    block

--- database/migrations/2022_06_01_134010_create_blocks_table.php
+++ database/migrations/2022_06_01_134010_create_blocks_table.php
@@ -17,11 +17,7 @@ return new class extends Migration
             $table->increments('id');
             $table->dateTime('start_date');
             $table->dateTime('end_date');
-<<<<<<< HEAD
-            $table->integer('duraition');
-=======
             $table->integer('duration');
->>>>>>> 567b9134d2f6cf06aa8ea94551dfa672561fc157
             $table->integer('service_provider_id')->unsigned();
             $table->foreign('service_provider_id')->references('id')->on('service_providers')->onDelete('cascade');
         });
