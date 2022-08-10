84d8e04c75c45990d7d70c984eb3546ff0690e58
unknown <bkerkz99@gmail.com>
2022-08-10 21:13:25 +0300

    provider middlewares

--- app/Http/Middleware/isProviderSuspended.php
+++ app/Http/Middleware/isProviderSuspended.php
@@ -19,7 +19,7 @@ class isProviderSuspended
     public function handle(Request $request, Closure $next)
     {
         $provider = ModelsServiceProvider::where('user_id', Auth::user()->id)->first();
-        if( $provider->account_status_id  == 2 )    return response(['error'=>'لا يمكنك الوصول'], 403);
+        if( $provider->account_status_id  == 2 )    return response(['error'=>'يجب تغيير حالة الحساب إلى نشط لتتمكن من تنفيذ هذه العملية'], 403);
         return $next($request);
     }
 }
